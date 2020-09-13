cc018ec2519b8ef424fdb8e9851922517bf3f06a
Vincent Moreau <vincentm498@gmail.com>
2020-09-13 17:22:53 +0200

    test commit

--- components/search.js
+++ components/search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Button, TextInput, Text } from 'react-native'
+import { View, Button, TextInput} from 'react-native'
 
 class Search extends React.Component{
     render(){
