207de57b216417693ed8907ee7661556fee89bdf
Vincent-Innovdata <vincent.innovdata@gmail.com>
2020-10-10 16:59:33 +0200

    Ajout de props fim

--- components/filmItem.js
+++ components/filmItem.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
-import FilmImage from './filmImage'
-import FilmDescription from './filmDescription'
-import FilmDate from './filmDate'
+import { StyleSheet, View, Text, Image  } from 'react-native'
 
 class FilmItem extends React.Component {
   render() {
+    const film = this.props.film
+    console.log(film)
     return (
       <View style={styles.main_container}>
-        <FilmImage/>
-        <View style={styles.container_text}>
-          <Text style={styles.title_text}>Titre du film de ouf</Text>
-          <FilmDescription/>
-          <FilmDate/>
+        <Image
+          style={styles.image}
+          source={{uri: "image"}}
+        />
+        <View style={styles.content_container}>
+          <View style={styles.header_container}>
+            <Text style={styles.title_text}>{film.title}</Text>
+            <Text style={styles.vote_text}>{film.vote_average}</Text>
+          </View>
+          <View style={styles.description_container}>
+            <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
+            {/* La propriété numberOfLines permet de couper un texte si celui-ci est trop long, il suffit de définir un nombre maximum de ligne */}
+          </View>
+          <View style={styles.date_container}>
+            <Text style={styles.date_text}>Sorti le {film.release_date}</Text>
+          </View>
         </View>
       </View>
       
@@ -22,16 +32,48 @@ class FilmItem extends React.Component {
 
 const styles = StyleSheet.create({
   main_container: {
-    height:250,
-    flexDirection:'row',
-    padding: 5
+    height: 190,
+    flexDirection: 'row'
+  },
+  image: {
+    width: 120,
+    height: 180,
+    margin: 5,
+    backgroundColor: 'gray'
+  },
+  content_container: {
+    flex: 1,
+    margin: 5
+  },
+  header_container: {
+    flex: 3,
+    flexDirection: 'row'
   },
   title_text: {
-    padding: 15
-  },
-  container_text:{
+    fontWeight: 'bold',
+    fontSize: 20,
     flex: 1,
-    justifyContent: 'space-between'
+    flexWrap: 'wrap',
+    paddingRight: 5
+  },
+  vote_text: {
+    fontWeight: 'bold',
+    fontSize: 26,
+    color: '#666666'
+  },
+  description_container: {
+    flex: 7
+  },
+  description_text: {
+    fontStyle: 'italic',
+    color: '#666666'
+  },
+  date_container: {
+    flex: 1
+  },
+  date_text: {
+    textAlign: 'right',
+    fontSize: 14
   }
 })
 
